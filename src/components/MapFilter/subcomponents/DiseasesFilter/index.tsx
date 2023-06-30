f80ca9ac49b1fbbd88e0a5b5b9f892e00a1ffbe1
Samsung Lim <123138598+samsunglim@users.noreply.github.com>
2023-06-30 20:23:28 +1000

    Update index.tsx
    const top100Films deleted

--- src/components/MapFilter/subcomponents/DiseasesFilter/index.tsx
+++ src/components/MapFilter/subcomponents/DiseasesFilter/index.tsx
@@ -13,16 +13,6 @@ import { InfoOutlined, Search } from "@mui/icons-material";
 import Tooltip, { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
 import { styled } from "@mui/material/styles";
 
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-];
-
 const CustomWidthTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
 ))({
